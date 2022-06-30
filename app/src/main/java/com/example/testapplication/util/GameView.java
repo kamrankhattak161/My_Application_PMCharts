d1851dbc84dd51da773caffc426372eefaee0d0f
kamran <m.imran@nitb.gov.pk>
2022-06-30 17:59:39 +0500

    Bullets addition

--- app/src/main/java/com/example/testapplication/util/GameView.java
+++ app/src/main/java/com/example/testapplication/util/GameView.java
@@ -3,7 +3,11 @@ package com.example.testapplication.util;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameView extends SurfaceView implements Runnable {
 
@@ -13,6 +17,7 @@ public class GameView extends SurfaceView implements Runnable {
     private Paint paint;
     private Flight flight;
     private Background background1, background2;
+    private List<Bullet> bullets;
 
     public GameView(Context context, int screenX, int screenY) {
         super(context);
@@ -23,7 +28,9 @@ public class GameView extends SurfaceView implements Runnable {
         background1 = new Background(screenX, screenY, getResources());
         background2 = new Background(screenX, screenY, getResources());
 
-        flight = new Flight(screenY, getResources());
+        flight = new Flight(this, screenY, getResources());
+
+        bullets = new ArrayList<>();
 
         background2.x = screenX;
 
@@ -51,6 +58,41 @@ public class GameView extends SurfaceView implements Runnable {
         if(background2.x + background2.background.getWidth() < 0) {
             background2.x = screenX;
         }
+        if (flight.isGoingUp)
+            flight.y -= 30;
+        else
+            flight.y += 30;
+
+        if (flight.y < 0)
+            flight.y = 0;
+
+        if (flight.y >= screenY - flight.height)
+            flight.y = screenY - flight.height;
+
+        List<Bullet> trash = new ArrayList<>();
+
+        for (Bullet bullet : bullets) {
+
+            if (bullet.x > screenX)
+                trash.add(bullet);
+
+            bullet.x += 50;
+
+            /*for (Bird bird : birds) {
+
+                if (Rect.intersects(bird.getCollisionShape(),
+                        bullet.getCollisionShape())) {
+
+                    score++;
+                    bird.x = -500;
+                    bullet.x = screenX + 500;
+                    bird.wasShot = true;
+
+                }
+
+            }*/
+
+        }
 
     }
 
@@ -61,6 +103,9 @@ public class GameView extends SurfaceView implements Runnable {
             canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
 
             canvas.drawBitmap(flight.getFlight(), flight.x, flight.y, paint);
+
+            for (Bullet bullet : bullets)
+            canvas.drawBitmap(bullet.bullet, bullet.x, bullet.y, paint);
 
             getHolder().unlockCanvasAndPost(canvas);
         }
@@ -89,5 +134,32 @@ public class GameView extends SurfaceView implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (event.getX() < screenX / 2) {
+                    flight.isGoingUp = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                flight.isGoingUp = false;
+                if (event.getX() > screenX / 2)
+                    flight.toShoot++;
+                break;
+        }
+
+        return true;
+    }
+
+
+    public void newBullet() {
+        Bullet bullet = new Bullet(getResources());
+        bullet.x = flight.x + flight.width;
+        bullet.y = flight.y + (flight.height / 2);
+        bullets.add(bullet);
     }
 }
